6bcd8819b0b97b481b2709541b8e1377c64258e3
Gipati <112897434+Gipati@users.noreply.github.com>
2022-11-21 18:50:40 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,6 @@ music.setBuiltInSpeakerEnabled(true)
 let låst = true
 OLED.init(128, 64)
 basic.forever(function () {
-    if (tinkercademy.PIR(DigitalPin.P0) && låst == true) {
-        basic.pause(2000)
-        music.setVolume(255)
-        music.playTone(880, music.beat(BeatFraction.Breve))
-    }
-})
-basic.forever(function () {
     OLED.clear()
     if (låst) {
         OLED.writeStringNewLine("laast")
@@ -19,9 +12,6 @@ basic.forever(function () {
 basic.forever(function () {
     if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P1)) {
         låst = true
-    }
-    if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P1)) {
-        låst = false
     }
     if (tinkercademy.ADKeyboard(ADKeys.C, AnalogPin.P0)) {
         låst = true
@@ -37,4 +27,14 @@ basic.forever(function () {
     while (låst == false) {
         music.stopAllSounds()
     }
+})
+basic.forever(function () {
+    if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P1)) {
+        låst = false
+    } else if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P0)) {
+        låst = true
+    }
+})
+basic.forever(function () {
+	
 })
